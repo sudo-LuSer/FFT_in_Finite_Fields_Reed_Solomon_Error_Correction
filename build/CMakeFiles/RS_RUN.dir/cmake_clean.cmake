383b29843d78a6a21c54e32eab6ab296c3f2f8fc
HITAR Nasr-Allah <Nasr-Allah.Hitar@bordeaux-inp.fr>
2026-03-09 09:46:38 +0100

    Corriger CMake

---
+++ build/CMakeFiles/RS_RUN.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RS_RUN.dir/src/RS_Decoder.cpp.o"
+  "CMakeFiles/RS_RUN.dir/src/RS_Decoder.cpp.o.d"
+  "CMakeFiles/RS_RUN.dir/src/RS_Encoder.cpp.o"
+  "CMakeFiles/RS_RUN.dir/src/RS_Encoder.cpp.o.d"
+  "CMakeFiles/RS_RUN.dir/src/RS_tools.cpp.o"
+  "CMakeFiles/RS_RUN.dir/src/RS_tools.cpp.o.d"
+  "CMakeFiles/RS_RUN.dir/src/main.cpp.o"
+  "CMakeFiles/RS_RUN.dir/src/main.cpp.o.d"
+  "RS_RUN"
+  "RS_RUN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RS_RUN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/RS_RUN.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RS_RUN.
+# This may be replaced when dependencies are built.
