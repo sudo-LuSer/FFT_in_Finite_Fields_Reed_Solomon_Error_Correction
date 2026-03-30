18bcda7b21b383bf452ee3a07157751c52787707
Nasr-allah <65375745+sudo-LuSer@users.noreply.github.com>
2026-03-30 16:00:21 +0200

    [UPDATE]
    mise à jour des fichiers !

--- src/RS_Decoder.cpp
+++ src/RS_Decoder.cpp
@@ -1,143 +1,195 @@
 #include "RS_Decoder.hpp"
 #include <algorithm>
 
-RS_Decoder::RS_Decoder(int n, int k, GaloisField &gf) : n(n), k(k), gf(gf) {
+#define GF_ADD(a,b) ((a) ^ (b))
+#define GF_SUB(a,b) ((a) ^ (b))
+
+RS_Decoder::RS_Decoder(int n, int k, GaloisField &gf)
+    : n(n), k(k), gf(gf)
+{
     t = (n - k) / 2;
+
+    AlphaPow_reg.resize(2*t);
+    AlphaInv_reg.resize(n);
+
+    for (int i = 0; i < 2*t; ++i)
+        AlphaPow_reg[i] = gf.get_alpha_to()[i+1];
+
+    for (int i = 0; i < n; ++i)
+        AlphaInv_reg[i] = gf.inv(gf.get_alpha_to()[i]);
 }
 
-int RS_Decoder::poly_eval(const std::vector<int>& poly, int x) {
+inline int RS_Decoder::poly_eval(const std::vector<int>& poly, int x)
+{
     int result = 0;
-    for (int i = (int)poly.size() - 1; i >= 0; --i) {
-        result = gf.add(gf.mul(result, x), poly[i]); // LFSR
-    }
+
+    for (int i = (int)poly.size() - 1; i >= 0; --i)
+        result = GF_ADD(gf.mul(result, x), poly[i]);
+
     return result;
 }
 
-std::vector<int> RS_Decoder::compute_syndromes(const std::vector<int>& received) {
-    std::vector<int> syndromes(2*t, 0);
-    for (int i = 0; i < 2*t; i++) {
-        int alpha_i = gf.get_alpha_to()[(i+1) % (gf.get_size() - 1)]; 
-        syndromes[i] = poly_eval(received, alpha_i);
-    }
-    return syndromes;
+std::vector<int> RS_Decoder::compute_syndromes(const std::vector<int>& received)
+{
+    std::vector<int> synd(2*t);
+
+    for (int i = 0; i < 2*t; ++i)
+        synd[i] = poly_eval(received, AlphaPow_reg[i]);
+
+    return synd;
 }
 
-std::vector<int> RS_Decoder::berlekamp_massey(const std::vector<int>& syndromes) {
-    int L = 0; 
-    std::vector<int> lambda(2*t + 1, 0);
-    lambda[0] = 1;
-    std::vector<int> b(2*t + 1, 0);
-    b[0] = 1;
-    int m = 1;
-    for (int r = 0; r < 2*t; r++) { // Voir algo dans le doc
-        int d = syndromes[r];
-        for (int i = 1; i <= L; i++) {
-            d = gf.add(d, gf.mul(lambda[i], syndromes[r - i]));
-        }
-        if(d==0){
-            m++;
-        }
-        else if(2*L <= r){
-            std::vector<int> T = lambda;
-            for (int i = 0; (i<b.size()) && ((i + m) < lambda.size()); i++){
-                lambda[i + m] = gf.sub(lambda[i + m], gf.mul(d, b[i]));
-            }
-            L = r + 1 - L; b = T; m = 1; 
+std::vector<int> RS_Decoder::berlekamp_massey(const std::vector<int>& s)
+{
+    const int N = 2*t;
 
-            int inv_d = gf.div(1, d);
-            for (int &val : b)
-                val = gf.mul(val, inv_d);
-        } 
-        else{
-            for (int i = 0; (i<b.size()) && ((i + m) < lambda.size()); i++){
-                lambda[i + m] = gf.sub(lambda[i + m], gf.mul(d, b[i]));
+    std::vector<int> C(N+1, 0), B(N+1, 0), D(N+1);
+
+    C[0] = 1;
+    B[0] = 1;
+
+    int L = 0, m = 1;
+
+    for (int r = 0; r < N; ++r) {
+        int d = s[r];
+
+        for (int i = 1; i <= L; ++i)
+            if (C[i] && s[r-i])
+                d ^= gf.mul(C[i], s[r-i]);
+
+        if (d != 0) {
+            std::copy(C.begin(), C.end(), D.begin());
+
+            for (int i = 0; i <= N-m; ++i)
+                if (B[i])
+                    C[i+m] ^= gf.mul(d, B[i]);
+
+            if (2*L <= r) {
+                L = r + 1 - L;
+
+                int inv_d = gf.inv(d);
+
+                for (int i = 0; i <= N; ++i)
+                    B[i] = gf.mul(D[i], inv_d);
+
+                m = 1;
+            } else {
+                ++m;
             }
-            m++;
+        } else {
+            ++m;
         }
     }
-    while (!lambda.empty() && lambda.back() == 0) 
-        lambda.pop_back();
-    return lambda;
+
+    C.resize(L+1);
+    return C;
 }
 
-std::vector<int> RS_Decoder::chien_search(const std::vector<int>& lambda, std::vector<int>& error_positions){
+std::vector<int> RS_Decoder::chien_search(
+    const std::vector<int>& lambda,
+    std::vector<int>& error_positions)
+{
     error_positions.clear();
+
     std::vector<int> X;
-    for (int i = 0; i < n; i++) {
-        int x = gf.div(1, gf.get_alpha_to()[i]);
-        if (poly_eval(lambda, x) == 0) {
+    X.reserve(t);
+
+    int L = lambda.size() - 1;
+
+    std::vector<int> reg(lambda);
+
+    for (int i = 0; i < n; i++){
+        int sum = 0;
+
+        for (int j = 0; j <= L; j++)
+            sum ^= reg[j];
+
+        if (sum == 0) {
             error_positions.push_back(i);
-            X.push_back(x);             
+            X.push_back(AlphaInv_reg[i]);
         }
+
+        for (int j = 1; j <= L; j++)
+                reg[j] = gf.mul(reg[j], AlphaInv_reg[j]);
     }
+
     return X;
 }
 
-std::vector<int> formal_derivative(const std::vector<int>& poly) {
-    std::vector<int> deriv(poly.size() - 1, 0);
-    for (size_t i = 1; i < poly.size(); i+=2) {
-        deriv[i-1] = poly[i]; 
-    }
-    return deriv;
+static std::vector<int> formal_derivative(const std::vector<int>& poly){
+    std::vector<int> d(poly.size() - 1, 0);
+
+    for (size_t i = 1; i < poly.size(); i += 2)
+        d[i-1] = poly[i];
+
+    return d;
 }
 
-std::vector<int> RS_Decoder::forney(const std::vector<int>& lambda, const std::vector<int>& omega, const std::vector<int>& error_positions, const std::vector<int>& X) {
-    std::vector<int> error_values(error_positions.size());
+std::vector<int> RS_Decoder::forney(
+    const std::vector<int>& lambda,
+    const std::vector<int>& omega,
+    const std::vector<int>& error_positions,
+    const std::vector<int>& X)
+{
+    std::vector<int> error_values(X.size());
+
     std::vector<int> lambda_prime = formal_derivative(lambda);
-    int idx = 0 ;
-    for (auto Xi : X) {
-        int omega_eval = poly_eval(omega, Xi);
-        int df_lambda_eval = poly_eval(lambda_prime, Xi);
-        int numer = omega_eval;
-        int denom = df_lambda_eval;
-        error_values[idx] = denom == 0 ? 0 : gf.div(numer, denom);
-        idx++;
+
+    for (size_t i = 0; i < X.size(); ++i) {
+        int Xi = X[i];
+
+        int num = poly_eval(omega, Xi);
+        int den = poly_eval(lambda_prime, Xi);
+
+        if (den == 0)
+            error_values[i] = 0;
+        else
+            error_values[i] = gf.mul(num, gf.inv(den));
     }
+
     return error_values;
 }
 
-std::vector<int> RS_Decoder::decode(const std::vector<int>& received) {
-    std::vector<int> syndromes = compute_syndromes(received);
-    bool all_zero = true;
-    for (int s : syndromes){
-        if (s != 0){
-            all_zero = false; 
-            break;
+std::vector<int> RS_Decoder::poly_mult(
+    const std::vector<int>& a,
+    const std::vector<int>& b)
+{
+    std::vector<int> res(a.size() + b.size() - 1, 0);
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (!a[i]) continue;
+
+        for (size_t j = 0; j < b.size(); ++j) {
+            if (!b[j]) continue;
+
+            res[i+j] ^= gf.mul(a[i], b[j]);
         }
     }
-    if (all_zero) 
-        return received;
-    std::vector<int> lambda = berlekamp_massey(syndromes);
 
-    std::vector<int> omega = poly_mult(syndromes, lambda);
-    omega.resize(2*t); // mod x^{2t}
+    return res;
+}
+
+std::vector<int> RS_Decoder::decode(const std::vector<int>& received){
+    std::vector<int> synd = compute_syndromes(received);
+
+    bool all_zero = true;
+    for (int s : synd) all_zero &= (s == 0);
+    if (all_zero) return received;
+
+    std::vector<int> lambda = berlekamp_massey(synd);
+
+    std::vector<int> omega = poly_mult(synd, lambda);
+    omega.resize(2*t);
 
     std::vector<int> error_positions;
     std::vector<int> X = chien_search(lambda, error_positions);
 
-    // if (error_positions.size() != lambda.size() - 1) {
-    //     throw std::runtime_error("Decoding failure: number of errors mismatch");
-    // }
-
     std::vector<int> error_values = forney(lambda, omega, error_positions, X);
 
     std::vector<int> corrected = received;
-    for (size_t idx = 0; idx < error_positions.size(); ++idx) {
-        int pos = error_positions[idx];
-        corrected[pos] = gf.sub(corrected[pos], error_values[idx]);
-    }
+
+    for (size_t i = 0; i < error_positions.size(); ++i)
+        corrected[error_positions[i]] ^= error_values[i];
 
     return corrected;
 }
-
-std::vector<int> RS_Decoder::poly_mult(const std::vector<int>& a, const std::vector<int>& b) {
-    std::vector<int> res(a.size() + b.size() - 1, 0);
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] == 0) continue;
-        for (size_t j = 0; j < b.size(); ++j) {
-            if (b[j] == 0) continue;
-            res[i+j] = gf.add(res[i+j], gf.mul(a[i], b[j]));
-        }
-    }
-    return res;
-} // peut etre à améliorer avec un FFT 
--- src/RS_Encoder.cpp
+++ src/RS_Encoder.cpp
@@ -1,87 +1,103 @@
 #include "RS_Encoder.hpp"
 #include "RS_tools.hpp"
 
-RS_Encoder::RS_Encoder(int n, int k) : n(n), k(k) {
-    if (n <= k) {
-        throw std :: invalid_argument("n must be greater than k");
-    }
-    t = (n - k) / 2; 
-    generator.clear();
-}
+// #include <aff3ct.hpp>
+// #include <algorithm>
+// #include <immintrin.h>
 
-void RS_Encoder::build_generator(GaloisField &gf) {
-    generator.clear();
+RS_Encoder::RS_Encoder(int n, int k, GaloisField &gf) : n(n), k(k), gf(&gf) {
+    if (n <= k)
+        throw std::invalid_argument("n must be greater than k");
+
+    t = (n - k) / 2;
+
+    // aff3ct::tools :: RS_polynomial_generator rs_g(n,t);
+    // generator = rs_g.get_g();
+
     generator.push_back(1);
-    const std :: vector <int> alpha_to_reg = gf.get_alpha_to();
-    for (int i = 1; i <= n-k; i++) {
-        int alpha_i = alpha_to_reg[i];
-        generator = poly_mult_by_binomial(generator, alpha_i, gf);
+
+    int start_deg = 1;   //remplacer par une LUT si nécessaire
+
+    for (int i = 1; i <= (n - k); i++) {
+        int alpha_i = gf.get_alpha_to()[i];
+        generator = poly_mult_by_binomial(generator, alpha_i);
+    }
+
+    int r = n - k;
+
+    mul_table.resize(r * (n + 1));
+
+    for (int j = 0; j < r; j++) {
+        for (int x = 0; x < n + 1; x++) {
+            mul_table[j * (n + 1) + x] = gf.mul(x, generator[j]);
+        }
     }
 }
 
-std :: vector<int> RS_Encoder::poly_mult_by_binomial(const std :: vector<int>& poly, int a, GaloisField &gf) {
-    size_t degree = poly.size();
-    std :: vector<int> result(degree + 1, 0);
-    
-    for (size_t j = 0; j < degree; j++) {
-        result[j + 1] = poly[j];
-    }
+std::vector<int> RS_Encoder::poly_mult_by_binomial(const std::vector<int>& poly, int a)
+{
+    std::vector<int> result(poly.size() + 1, 0);
 
-    for (size_t j = 0; j < degree; j++) {
-        result[j] = poly[j] != 0 ? gf.add(result[j], gf.mul(poly[j], a)) : result[j];
-    }
+    result[0] = gf->mul(poly[0], a);
+
+    for(size_t i = 1; i < poly.size(); i++)
+        result[i] = GF_ADD(gf->mul(poly[i], a), poly[i-1]);
+
+    result[poly.size()] = poly.back();
+
     return result;
 }
 
-std :: vector<int> RS_Encoder::poly_div(const std :: vector<int>& dividend, const std :: vector<int>& divisor, GaloisField &gf) {
-    std :: vector<int> remainder = dividend;
-    int deg_d = (int)divisor.size() - 1;
+// std :: vector<int> RS_Encoder::poly_div(const std :: vector<int>& dividend, const std :: vector<int>& divisor) {
+//     std :: vector<int> remainder = dividend;
+//     int deg_d = (int)divisor.size() - 1;
     
-    if (divisor.empty() || divisor[deg_d] == 0) {
-        throw std :: invalid_argument("Divisor polynomial is zero or invalid");
-    }
+//     if (divisor.empty() || divisor[deg_d] == 0) {
+//         throw std :: invalid_argument("Divisor polynomial is zero or invalid");
+//     }
     
-    while(remainder.size() >= divisor.size() && !remainder.empty()) {
-        int deg_r = (int)remainder.size() - 1;
-        int shift = deg_r - deg_d;
+//     while(remainder.size() >= divisor.size() && !remainder.empty()) {
+//         int deg_r = (int)remainder.size() - 1;
+//         int shift = deg_r - deg_d;
         
-        int coef = gf.div(remainder[deg_r], divisor[deg_d]);
+//         int coef = gf->div(remainder[deg_r], divisor[deg_d]);
         
-        for(int i = 0; i <= deg_d; i++) {
-            if (i + shift < (int)remainder.size()) {
-                remainder[i + shift] = gf.sub(remainder[i + shift], gf.mul(coef, divisor[i]));
-            }
-        }
+//         for(int i = 0; i <= deg_d; i++) {
+//             if (i + shift < (int)remainder.size()) {
+//                 remainder[i + shift] = gf->sub(remainder[i + shift], gf->mul(coef, divisor[i]));
+//             }
+//         }
         
-        while(!remainder.empty() && remainder.back() == 0) {
-            remainder.pop_back();
-        }
-    }
+//         while(!remainder.empty() && remainder.back() == 0) {
+//             remainder.pop_back();
+//         }
+//     }
     
-    if (remainder.empty()) {
-        return {0};
-    }
+//     if (remainder.empty()) {
+//         return {0};
+//     }
     
-    return remainder;
-}
+//     return remainder;
+// }
 
-std :: vector<int> RS_Encoder::encode(const std :: vector<int>& message, GaloisField &gf) {
-    if (message.size() != (size_t)k) {
-        throw std :: invalid_argument("Message length must be k = " + std :: to_string(k));
+void RS_Encoder::encode(const std::vector<int>& message, std :: vector <int> &codeword){
+    if (message.size() != (size_t)k)
+        throw std::invalid_argument("Message length must be k = " + std::to_string(k));
+
+    int r = n - k;
+    std::vector<int> parity(r, 0);
+
+    for (int i = k - 1; i >= 0; i--){
+        int feedback = GF_ADD(message[i], parity[r - 1]);
+
+        for (int j = r - 1; j > 0; j--){
+            parity[j] = GF_ADD(parity[j - 1], mul_table[j*(n+1) + feedback]);
+        }
+
+        parity[0] = mul_table[feedback];
     }
-    
-    std :: vector<int> mx_poly(message.begin(), message.end());
-    mx_poly.insert(mx_poly.begin(), n-k, 0);
-    std :: vector<int> remainder = poly_div(mx_poly, generator, gf);
-    
-    remainder.resize(n - k, 0);
-    
-    std :: vector<int> codeword = remainder;
-    codeword.insert(codeword.end(), message.begin(), message.end());
-    
-    if (codeword.size() != (size_t)n) {
-        throw std :: runtime_error("Codeword length incorrect");
-    }
-    
-    return codeword;
+
+    codeword.resize(n);
+    std::copy(parity.begin(), parity.end(), codeword.begin());
+    std::copy(message.begin(), message.end(), codeword.begin() + r);
 }
