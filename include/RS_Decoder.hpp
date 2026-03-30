15cda50741cfce846e407c5068763b69613c0319
Nasr-allah <65375745+sudo-LuSer@users.noreply.github.com>
2026-03-30 16:03:40 +0200

    mise à jour des définitions

--- include/RS_Decoder.hpp
+++ include/RS_Decoder.hpp
@@ -1,6 +1,6 @@
 #ifndef RS_DECODER_HPP
 #define RS_DECODER_HPP
-
+#define RS_DECODER_HPP
 #include <vector>
 #include "RS_tools.hpp"
 #include "RS_Encoder.hpp"
@@ -9,7 +9,6 @@ class RS_Decoder {
 private:
     int n, k, t;
     GaloisField &gf;
-
 public:
     RS_Decoder(int n, int k, GaloisField &gf);
     std::vector<int> compute_syndromes(const std::vector<int>& received);
@@ -18,7 +17,10 @@ public:
     std::vector<int> forney(const std::vector<int>& lambda, const std::vector<int>& omega, const std::vector<int>& error_positions, const std::vector<int>& X);
     std::vector<int> decode(const std::vector<int>& received);
     std::vector<int> poly_mult(const std::vector<int>& a, const std::vector<int>& b);
-    int poly_eval(const std::vector<int>& poly, int x);
+    inline int poly_eval(const std::vector<int>& poly, int x);
+protected: 
+    std :: vector <int> AlphaPow_reg;
+    std :: vector <int> AlphaInv_reg; 
 };
 
 #endif
--- include/RS_Encoder.hpp
+++ include/RS_Encoder.hpp
@@ -1,6 +1,6 @@
 #ifndef RS_ENCODER_HPP
 #define RS_ENCODER_HPP
-
+#define RS_ENCODER_HPP
 #include <vector>
 
 class GaloisField;
@@ -11,17 +11,19 @@ private:
     int k;          
     int t;             
     std :: vector<int> generator; 
-    
+    GaloisField *gf; 
+    std::vector<int> mul_table; 
 public: 
-    RS_Encoder(int n, int k);
+    RS_Encoder(int n, int k, GaloisField &gf);
     
     const std :: vector<int>& get_generator() const { return generator; }
     int get_degree() const { return generator.empty() ? 0 : (int)generator.size() - 1; }
     
-    void build_generator(GaloisField &gf);
-    std :: vector<int> encode(const std :: vector<int>& message, GaloisField &gf);
-    std :: vector<int> poly_div(const std :: vector<int>& dividend, const std :: vector<int>& divisor, GaloisField &gf);
-    std :: vector<int> poly_mult_by_binomial(const std :: vector<int>& poly, int a, GaloisField &gf);
+    std :: vector<int> poly_mult_by_binomial(const std :: vector<int>& poly, int a);
+
+    void encode(const std::vector<int>& message, std :: vector <int> &codeword);
+
+    // std :: vector<int> poly_div(const std :: vector<int>& dividend, const std :: vector<int>& divisor);
 };
 
 #endif
